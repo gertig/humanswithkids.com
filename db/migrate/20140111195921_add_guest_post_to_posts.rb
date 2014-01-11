4cc13af3ab5ab63978febce8fa6885b95e3e2228
AndrewGertig <andrew.gertig@gmail.com>
2014-01-11 15:00:04 -0500

    add guest boolean to posts for guest posts

---
+++ db/migrate/20140111195921_add_guest_post_to_posts.rb
@@ -0,0 +1,5 @@
+class AddGuestPostToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :guest, :boolean, :default => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111162951) do
+ActiveRecord::Schema.define(version: 20140111195921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20140111162951) do
     t.string   "image_url_string"
     t.boolean  "url_contains_date", default: false
     t.boolean  "featured",          default: false
+    t.boolean  "guest",             default: false
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", using: :btree
